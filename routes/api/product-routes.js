9961fe0713d27c5ff1db6f9b09c83983749eb86b
oscalenghe <42279508+oscalenghe@users.noreply.github.com>
2023-03-28 16:27:54 -0400

    .destroy added to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,7 +106,13 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: { id: req.params.id,
+    },
+  })
+  .then((products) => {res.json(products);
+  }).catch((err) => res.json(err));
+
 });
 
 module.exports = router;
