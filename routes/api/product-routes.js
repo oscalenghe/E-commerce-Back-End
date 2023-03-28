2e8f51728af89e6f7714cd86606e665ba6248144
oscalenghe <42279508+oscalenghe@users.noreply.github.com>
2023-03-28 16:21:35 -0400

    added .findAll and .findOne to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category,
+    {model: Tag,
+    through: ProductTag,},
+  ],
+  })
+  .then((products) => {
+    res.json(products);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id:req.params.id,
+    }, include: [Category,
+    {model: Tag,
+    through: ProductTag,},
+  ],
+  })
+  .then((product) => {
+    res.json(product);
+  });
 });
 
 // create new product
