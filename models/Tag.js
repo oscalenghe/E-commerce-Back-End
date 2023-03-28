9154eb26ebacbf34e7fb5122aef62c476166c719
oscalenghe <42279508+oscalenghe@users.noreply.github.com>
2023-03-28 15:32:21 -0400

    completed Tag.js model, finished all models

--- models/Tag.js
+++ models/Tag.js
@@ -16,7 +16,15 @@ String
 */
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
